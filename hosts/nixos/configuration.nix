2cbc8b8f102147ef9021b683b8098365f1e163bb
Jerrik <jerrikcode+nix@gmail.com>
2025-06-07 14:09:09 -0700

    add more packages

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -69,17 +69,27 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+   alacritty
+   bat
+   btop
    distrobox
+   diff-so-fancy
    fzf
    gcc
    git
+   lazydocker
    lazygit
+   lazysql
    neovim
+   newsboat
    ripgrep
    tmux
    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
    #vimPlugins.LazyVim
    wget
+   yazi
+   zsh-syntax-highlighting
+   zsh-vi-mode
    zsh
   ];
 
