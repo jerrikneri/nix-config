15946a3082f8547c9d3aca5d9ae05d8149294601
Jerrik <jerrikcode+nix@gmail.com>
2025-06-06 19:20:04 -0700

    add distrobox

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -69,6 +69,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+   distrobox
    fzf
    gcc
    git
@@ -76,6 +77,7 @@
    neovim
    tmux
    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   vimPlugins.LazyVim
    wget
    zsh
   ];
